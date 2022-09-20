cc377291124ffd5b865242ac3fc651a1b011b50c
Emili0 <emiliothomasking@gmail.com>
2022-09-19 22:14:01 -0700

    minor change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
