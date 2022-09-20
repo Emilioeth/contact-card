fbfbf0f093a9f01e8e4b03d69a3941cff4d17d3d
Emili0 <emiliothomasking@gmail.com>
2022-09-19 22:01:00 -0700

    added install functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -15,6 +15,24 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+// Install button
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
 // On load functionality
 window.addEventListener('load', function () {
   initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {InjectManifest} = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
 const path = require('path');
 
 module.exports = {
@@ -18,6 +20,28 @@ plugins: [
   new InjectManifest({
     swSrc: './src/sw.js',
     swDest: 'service-worker.js',
+  }),
+  new WebpackPwaManifest({
+    name: 'Contact Cards Application',
+    short_name: 'Contact Cards',
+    description: 'Keep track of contacts!',
+    background_color: '#7eb4e2',
+    theme_color: '#7eb4e2',
+    start_url: './',
+    publicPath: './',
+    icons: [
+      {
+        src: path.resolve('src/images/icon-manifest.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons'),
+      },
+      {
+        src: path.resolve('src/images/icon-manifest.png'),
+        size: '1024x1024',
+        destination: path.join('assets', 'icons'),
+        purpose: 'maskable'
+      }
+    ],
   }),
 ],
     module: {
